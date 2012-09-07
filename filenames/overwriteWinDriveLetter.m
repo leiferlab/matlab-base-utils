b3a67ad05962943f830065469f7630830e446c9a
Andrew Leifer <leifer@fas.harvard.edu>
2012-09-07 16:32:38 -0400

    consolodating

--- filenames/overwriteWinDriveLetter.m
+++ filenames/overwriteWinDriveLetter.m
@@ -1,7 +1,7 @@
-function newfile=overwriteWinDriveLetter(fullfile,newDriveLetter)
+function newfile=swapWinDriveLetter(fullfile,newDriveLetter)
 %Swap out the drive letter for newDriveLetter
 %EXAMPLE
-% newfile=overwriteWinDriveLetter('F:\My\Folder\file.txt','G')
+% newfile=swapWinDriveLetter('F:\My\Folder\file.txt','G')
 % newfile=
 %       G:\My\Folder\file.txt
 %
@@ -9,6 +9,6 @@ function newfile=overwriteWinDriveLetter(fullfile,newDriveLetter)
 % leifer@princeton.edu
 % 7 September 2012
 
-[~,remain]=strtok(fullfile);
+[~,remain]=strtok(fullfile,':');
  newfile=[newDriveLetter remain];
 end
