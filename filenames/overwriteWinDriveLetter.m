d635c4de2ec2d1a64581338a024a28042b3b8eb4
Andrew Leifer <leifer@fas.harvard.edu>
2013-03-02 07:51:30 -0500

    updated overwriteWinDriveLetter to handle empty dadatrives

--- filenames/overwriteWinDriveLetter.m
+++ filenames/overwriteWinDriveLetter.m
@@ -1,14 +1,22 @@
-function newfile=swapWinDriveLetter(fullfile,newDriveLetter)
+function newfile=overwriteWinDriveLetter(fullfile,newDriveLetter)
+%newfile=overwriteWinDriveLetter(fullfile,newDriveLetter) 
+%
 %Swap out the drive letter for newDriveLetter
 %EXAMPLE
-% newfile=swapWinDriveLetter('F:\My\Folder\file.txt','G')
+% newfile=overwriteWinDriveLetter('F:\My\Folder\file.txt','G')
 % newfile=
 %       G:\My\Folder\file.txt
+%
+% Note if newDriveLetter is empty, the function returns the fullfile
 %
 % Andrew Leifer
 % leifer@princeton.edu
 % 7 September 2012
 
+if isempty(newDriveLetter)
+    newfile=fullfile;
+else
 [~,remain]=strtok(fullfile,':');
  newfile=[newDriveLetter remain];
+end
 end
